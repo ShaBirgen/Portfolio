bd3b5915cee1695188111e1bf9264963892a95b8
ShaBirgen <sharoncherotich112@gmail.com>
2024-05-28 21:14:50 +0300

    Homepage

--- Portfolio/src/Components/Home.jsx
+++ Portfolio/src/Components/Home.jsx
@@ -1,24 +1,33 @@
 import Button from './Button'
 import './Home.scss'
 import Profile from '../assets/Birgen.png'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import Homepage from './Homepage'
 
 function Home() {
   return (
-    <div className='mainHome'>
-      <div className="mainLeft">
-        <h3>Hello!</h3>
-        <p>I'm Sharon Cherotich Birgen</p>
-        <h2>Quality Assurance Engineer</h2>
-        <div className="buttons">
-        <Button msg= 'Hire Me'/>
-        <Button msg='My Work'/>
+    <><div className="routes">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path='/home' element={<Homepage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div><div className='mainHome'>
+        <div className="mainLeft">
+          <h3>Hello!</h3>
+          <p>I'm Sharon Cherotich Birgen</p>
+          <h2>Quality Assurance Engineer</h2>
+          <div className="buttons">
+            <Button msg='Hire Me' />
+            <Button msg='My Work' />
+          </div>
         </div>
-      </div>
 
-      <div className="mainRight">
-        <img className='profile' src={Profile} alt="profile" />
-      </div>
-    </div>
+        <div className="mainRight">
+          <img className='profile' src={Profile} alt="profile" />
+        </div>
+      </div></>
   )
 }
 
---
+++ Portfolio/src/Components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='main'>
+      <h1>This is my homepage</h1>
+    </div>
+  )
+}
+
+export default Homepage
