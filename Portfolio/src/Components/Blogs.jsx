28f34ff419670adc26c69663c3b2ca1bbc7b43cf
ShaBirgen <sharoncherotich112@gmail.com>
2024-06-06 16:01:16 +0300

    Blog

--- Portfolio/src/Components/Blogs.jsx
+++ Portfolio/src/Components/Blogs.jsx
@@ -1,9 +1,22 @@
 import './Blogs.scss'
+import { useState } from 'react'
+
 
 function Blogs() {
+  const [blogs, setBlogs]= useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
   return (
-    <div>
-      <h1>Life is interesting. Join mine as i give you my daily life routines.</h1>
+    <div className='main'>
+      {blogs.map((blog)=>(
+        <div className="article" key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p><small>Written by: {blog.author}</small></p>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   )
 }
